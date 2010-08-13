216c9aa95deb1bc71f7dff4fd967af17510ebc9c
Jon Udell <jonu@microsoft.com>
2010-08-13 07:50:18 -0400

    formatting

--- elmcityutils/ObjectUtils.cs
+++ elmcityutils/ObjectUtils.cs
@@ -17,63 +17,65 @@ using System.Collections.Generic;
 
 namespace ElmcityUtils
 {
-    public static class ObjectUtils
-    {
+	public static class ObjectUtils
+	{
 
-        public static Dictionary<string, object> ObjToDictObj(Object o)
-        {
-            Dictionary<string, object> dict_obj = new Dictionary<string, object>();
-            var type = o.GetType();
-            foreach (var property in type.GetProperties())
-                dict_obj[property.Name] = property.GetValue(o, index: null);
-            return dict_obj;
-        }
+		public static Dictionary<string, object> ObjToDictObj(Object o)
+		{
+			Dictionary<string, object> dict_obj = new Dictionary<string, object>();
+			var type = o.GetType();
+			foreach (var property in type.GetProperties())
+				dict_obj[property.Name] = property.GetValue(o, index: null);
+			return dict_obj;
+		}
 
-        public static Object DictObjToObj(Dictionary<string, object> dict_obj, Type type)
-        {
-            var o = Activator.CreateInstance(type);
+		public static Object DictObjToObj(Dictionary<string, object> dict_obj, Type type)
+		{
+			var o = Activator.CreateInstance(type);  // create object
 
-            if (type.GetProperties() == null)
-            {
-                GenUtils.LogMsg("exception", "DictObjToObj: " + type.Name, "target type does not define properties");
-                return o;
-            }
+			if (type.GetProperties() == null)
+			{
+				GenUtils.LogMsg("exception", "DictObjToObj: " + type.Name, 
+					"target type does not define properties");
+				return o;
+			}
 
-            foreach (var key in dict_obj.Keys)
-            {
-                try
-                {
-                    type.GetProperty(key).SetValue(o, dict_obj[key], index: null);
-                }
-                catch (NullReferenceException)
-                {
-                    // this is normal since an azure table includes PartitionKey, RowKey, and Timestamp which
-                    // will not be part of the object
-                }
-                catch (Exception e)
-                {
-                    GenUtils.LogMsg("exception", "DictObjToObj: " + type.Name, e.Message + e.StackTrace);
-                }
-            }
+			foreach (var key in dict_obj.Keys)
+			{
+				try                                  // set properties
+				{
+					type.GetProperty(key).SetValue(o, dict_obj[key], index: null);
+				}
+				catch (NullReferenceException)
+				{
+					// this is normal since an azure table includes PartitionKey, RowKey, 
+					// and Timestamp which will not map into the object
+				}
+				catch (Exception e)
+				{
+					GenUtils.LogMsg("exception", "DictObjToObj: " + type.Name,
+						e.Message + e.StackTrace);
+				}
+			}
 
-            return o;
-        }
+			return o;
+		}
 
-        public static Dictionary<string, string> DictObjToDictStr(Dictionary<string, object> dict_obj)
-        {
-            Dictionary<string, string> dict_str = new Dictionary<string, string>();
-            foreach (string key in dict_obj.Keys)
-                dict_str.Add(key, dict_obj[key].ToString());
-            return dict_str;
-        }
+		public static Dictionary<string, string> DictObjToDictStr(Dictionary<string, object> dict_obj)
+		{
+			Dictionary<string, string> dict_str = new Dictionary<string, string>();
+			foreach (string key in dict_obj.Keys)
+				dict_str.Add(key, dict_obj[key].ToString());
+			return dict_str;
+		}
 
-        public static Dictionary<string, object> DictStrToDictObj(Dictionary<string, string> dict_str)
-        {
-            Dictionary<string, object> dict_obj = new Dictionary<string, object>();
-            foreach (var key in dict_str.Keys)
-                dict_obj.Add(key, (object)dict_str[key]);
-            return dict_obj;
-        }
+		public static Dictionary<string, object> DictStrToDictObj(Dictionary<string, string> dict_str)
+		{
+			Dictionary<string, object> dict_obj = new Dictionary<string, object>();
+			foreach (var key in dict_str.Keys)
+				dict_obj.Add(key, (object)dict_str[key]);
+			return dict_obj;
+		}
 
-    }
+	}
 }
