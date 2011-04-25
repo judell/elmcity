a07cdf6d2c586ef675ddda70822bcf462321edaa
Jon Udell <jonu@microsoft.com>
2011-04-25 08:27:44 -0400

    Levenshtein for dupe detection

--- elmcityutils/GenUtils.cs
+++ elmcityutils/GenUtils.cs
@@ -386,5 +386,78 @@ namespace ElmcityUtils
 		}
 
 		#endregion
+
+		#region str
+
+		public static string Shorter(string s1, string s2)
+		{
+			return s1.Length < s2.Length ? s1 : s2;
+		}
+
+		public static string Longer(string s1, string s2)
+		{
+			return s1.Length > s2.Length ? s1 : s2;
+		}
+
+		#endregion str
+
+	}
+
+	public static class LevenshteinDistance
+	{
+		public static int Compute(string s, string t)
+		{
+			int n = s.Length;
+			int m = t.Length;
+			int[,] d = new int[n + 1, m + 1];
+
+			// Step 1
+			if (n == 0)
+			{
+				return m;
+			}
+
+			if (m == 0)
+			{
+				return n;
+			}
+
+			// Step 2
+			for (int i = 0; i <= n; d[i, 0] = i++)
+			{
+			}
+
+			for (int j = 0; j <= m; d[0, j] = j++)
+			{
+			}
+
+			// Step 3
+			for (int i = 1; i <= n; i++)
+			{
+				//Step 4
+				for (int j = 1; j <= m; j++)
+				{
+					// Step 5
+					int cost = (t[j - 1] == s[i - 1]) ? 0 : 1;
+
+					// Step 6
+					d[i, j] = Math.Min(
+						Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1),
+						d[i - 1, j - 1] + cost);
+				}
+			}
+			// Step 7
+			return d[n, m];
+		}
+	}
+
+	public static class MathHelpers
+	{
+		public static bool Between(this int num, int lower, int upper, bool inclusive = false)
+		{
+			return inclusive
+				? lower <= num && num <= upper
+				: lower < num && num < upper;
+		}
 	}
 }
