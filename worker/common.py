f48a75aad6e0b0debadb78bfdcd7a717fa3367f5
Jon Udell <jonu@microsoft.com>
2011-04-25 08:31:33 -0400

    noop

--- worker/common.py
+++ worker/common.py
@@ -3,22 +3,18 @@ def get_resource_dirs():
 
 def get_local_storage():
   return [dir for dir in get_resource_dirs() if dir.endswith('LocalStorage1')][0]
-
  
 def get_diagnostic_store():
-  return [dir for dir in get_resource_dirs() if dir.endswith('DiagnosticStore')][0]  
-
+  return [dir for dir in get_resource_dirs() if dir.endswith('DiagnosticStore')][0] 
 
 def get_log_storage():
   return [d for d in System.IO.Directory.GetDirectories("%s/LogFiles/Web" % get_diagnostic_store())][0]  
-
 
 def show_rules(ds,log):
   rules = ds.GetAccessRules(True, True, System.Type.GetType('System.Security.Principal.NTAccount'))
   log.write('%s rules\n' % rules.Count)
   for rule in rules:
     log.write('%s %s %s\n' % (rule.IdentityReference.Value, rule.AccessControlType, rule.FileSystemRights))  
-
 
 def set_permissions(directory, who, rights, inheritance, propagation, access_type):
   try:
@@ -32,7 +28,6 @@ def set_permissions(directory, who, rights, inheritance, propagation, access_typ
   except:
     log.write('cannot set permissions on %s' % directory )     
 
-
 def get_process_owner():
   mos = System.Management.ManagementObjectSearcher("SELECT * from Win32_Process")
   for mo in mos.Get():
@@ -41,7 +36,6 @@ def get_process_owner():
     name = mo.GetPropertyValue('Name')
     if ( name == System.Diagnostics.Process.GetCurrentProcess().MainModule.ModuleName ):
       return [ oi[0], oi[1] ]
-
 
 def format_traceback():    
   exc_type, exc_value, exc_traceback = sys.exc_info()
