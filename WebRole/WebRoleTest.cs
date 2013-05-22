866a564958aafad71071c93dc77f7b75ebf0edb5
Jon Udell <jonu@microsoft.com>
2013-05-22 11:21:57 -0400

    new tests

---
+++ WebRole/WebRoleTest.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using ElmcityUtils;
+using Newtonsoft.Json;
+using System.Xml;
+
+namespace WebRole
+{
+	[TestFixture]
+	public class WebRoleTest
+	{
+
+		private string hub = CalendarAggregator.Configurator.testid;
+		private string category = "test_category";
+		private string host = "http://" + ElmcityUtils.Configurator.appdomain;
+		private string path;
+
+		public WebRoleTest()
+		{
+			this.path = host + "/" + hub;
+		}
+
+		[Test]
+		public void EventsAsHtml()
+		{
+			var uri = new Uri(path);
+			var html = HttpUtils.FetchUrl(uri).DataAsString();
+			Assert.That(html.Contains(@"<span class=""st"" property=""v:startDate"" content=""9999-01-01T09:01"">Fri 09:01 AM</span>"));
+		}
+
+		[Test]
+		public void EventsAsHtmlView()
+		{
+			var uri = new Uri(path + "?view=test_category");
+			var html = HttpUtils.FetchUrl(uri).DataAsString();
+			Assert.That(html.Contains(@"<span class=""st"" property=""v:startDate"" content=""9999-01-01T09:01"">Fri 09:01 AM</span>"));
+		}
+
+		[Test]
+		public void EventsAsXml()
+		{
+			var uri = new Uri(path + "/xml");
+			var r = HttpUtils.FetchUrl(uri);
+			var doc = XmlUtils.XmlDocumentFromHttpResponse(r);
+			XmlNodeList nodes = doc.SelectNodes("//event");
+			Assert.That(nodes.Count == 2);
+		}
+
+		[Test]
+		public void EventsAsXmlView()
+		{
+			var uri = new Uri(path + "/xml?view=test_category");
+			var r = HttpUtils.FetchUrl(uri);
+			var doc = XmlUtils.XmlDocumentFromHttpResponse(r);
+			XmlNodeList nodes = doc.SelectNodes("//event");
+			Assert.That(nodes.Count == 1);
+		}
+
+		[Test]
+		public void EventsAsJson()
+		{
+			var uri = new Uri(path + "/json");
+			var json = HttpUtils.FetchUrl(uri).DataAsString();
+			var list_of_dict = (List<Dictionary<string,object>>) JsonConvert.DeserializeObject <List<Dictionary<string, object>>>(json);
+			Assert.That(list_of_dict.Count == 2);
+		}
+
+		[Test]
+		public void EventsAsJsonView()
+		{
+			var uri = new Uri(path + "/json?view=test_category");
+			var json = HttpUtils.FetchUrl(uri).DataAsString();
+			var list_of_dict = (List<Dictionary<string, object>>)JsonConvert.DeserializeObject<List<Dictionary<string, object>>>(json);
+			Assert.That(list_of_dict.Count == 1);
+		}
+
+
+	}
+
+}
