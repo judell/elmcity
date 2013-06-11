6fd4e3e8cebd395b0da6cc935559beec3e9c3b42
Jon Udell <jonu@microsoft.com>
2013-06-11 10:46:06 -0400

    tweak layout

--- WebRole/Global.asax.cs
+++ WebRole/Global.asax.cs
@@ -136,7 +136,7 @@ namespace WebRole
 
     public class ElmcityApp : HttpApplication
     {
-        public static string version = "2507";
+        public static string version = "2509";
 
         public static string procname = System.Diagnostics.Process.GetCurrentProcess().ProcessName;
         public static int procid = System.Diagnostics.Process.GetCurrentProcess().Id;
