053024935aac6967f85f57c0422281ae68fd0652
Jon Udell <jonu@microsoft.com>
2013-07-31 13:05:00 -0400

    refactored for use with typescript

---
+++ ElmcityAdmin/extensions.js
@@ -0,0 +1,25 @@
+Date.prototype.addDays = function(days) {
+    this.setDate(this.getDate()+days);
+} 
+
+String.prototype.replaceAt=function(index, char) {
+    return this.substr(0, index) + char + this.substr(index+char.length);
+}
+
+String.prototype.startsWith = function (str){
+    return this.indexOf(str) == 0;
+};
+
+String.prototype.contains = function (str){
+    return this.indexOf(str) != -1;
+};
+
+String.prototype.endsWith = function (str){
+    return this.indexOf(str) == this.length - str.length - 1;
+};
+
+if(!String.prototype.trim) {
+  String.prototype.trim = function () {
+    return this.replace(/^\s+|\s+$/g,'');
+  };
+}
