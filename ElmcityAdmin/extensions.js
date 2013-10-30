b37ae4e964d86ca33e3299b9cd6da02980557450
Jon Udell <jonu@microsoft.com>
2013-10-30 13:42:09 -0400

    support for ie

--- ElmcityAdmin/extensions.js
+++ ElmcityAdmin/extensions.js
@@ -23,3 +23,15 @@ if(!String.prototype.trim) {
     return this.replace(/^\s+|\s+$/g,'');
   };
 }
+
+
+if(!Array.indexOf){
+  Array.prototype.indexOf = function(obj){
+   for(var i=0; i<this.length; i++){
+    if(this[i]==obj){
+     return i;
+    }
+   }
+   return -1;
+  }
+}
